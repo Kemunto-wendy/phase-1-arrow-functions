5f0017c2ed77796f339abdd046ee963bf631644e
JOY <joy.kemunto@student.moringaschool.com>
2022-06-08 16:24:16 +0300

    commit tree b873e18840eecdf2234d02de306f4c5bc6be4854

---
+++ index.js
@@ -0,0 +1,12 @@
+//My code is written below
+//function called divide and it divides 2000/100 which results to 20
+let divide = () => 2000/100
+
+//function square and takes x multiplies it by itself
+let square = (x) => x * x
+//let square = x => x * x;
+
+//function add two parameters and adds them together a,b
+let add = (a, b) =>a + b
+//let add = (a, b) => a + b;
+
